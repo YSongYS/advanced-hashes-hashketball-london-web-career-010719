9a6a6a23668e8cc533d8b198822106094ab636f1
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-05 18:45:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,7 +30,7 @@ end
 def num_points_scored(player_name)
   game_hash.values.each do |team|
     team[:players].each do |player|
-      return player[:value] if player.has_value?(player_name)
+      return player[:points] if player.has_value?(player_name)
     end
   end
 end
